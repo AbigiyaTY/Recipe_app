0ce6d746898d743e317e3bb4f6afdca4677c53f4
X-Elie-X <103565978+X-Elie-X@users.noreply.github.com>
2023-01-02 16:52:49 +0200

    fixing linters errors

--- app/models/food.rb
+++ app/models/food.rb
@@ -1,4 +1,4 @@
 class Food < ApplicationRecord
-    belongs_to :users, class_name: 'User', foreign_key: :User_ID
-    has_many :recipe_foods, foreign_key: :Food_ID
+  belongs_to :users, class_name: 'User', foreign_key: :User_ID
+  has_many :recipe_foods, foreign_key: :Food_ID
 end
--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,4 +1,4 @@
 class Recipe < ApplicationRecord
-    belongs_to :users, class_name: 'User', foreign_key: :User_ID
-    has_many :recipe_foods, foreign_key: :Recipe_ID
+  belongs_to :users, class_name: 'User', foreign_key: :User_ID
+  has_many :recipe_foods, foreign_key: :Recipe_ID
 end
