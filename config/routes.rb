cbc60639661df83e0260ce74e1e50f7bd2642904
X-Elie-X <103565978+X-Elie-X@users.noreply.github.com>
2023-01-03 22:53:51 +0200

    Adding long in and user and recite

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,13 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  root "foods#index"
-
-  resources :foods
-  resources :recipes do
-    resources :recipe_foods
+  devise_for :users
+  get 'recipes/index', to: 'recipes#public'
+  resources :users do
+    resources :recipes
   end
-  resources :public_recipes
-  resources :general_shopping_list
+  resources :users do
+    resources :foods
+  end
+  
+  root "recipes#public"
 end
