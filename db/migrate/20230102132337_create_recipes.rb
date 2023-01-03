2cfec7f670f425b0db1c70bfc2886c68740bac4d
Abigiya Tadesse <Abigiyatadesse23@gmail.com>
2023-01-03 11:32:32 +0300

    Fix database

--- db/migrate/20230102132337_create_recipes.rb
+++ db/migrate/20230102132337_create_recipes.rb
@@ -2,10 +2,10 @@ class CreateRecipes < ActiveRecord::Migration[7.0]
   def change
     create_table :recipes do |t|
       t.string :Name
-      t.integer :Preparation_Time
-      t.integer :Cooking_Time
-      t.string :Decription
-      t.string :Public
+      t.string :Preparation_Time
+      t.string :Cooking_Time
+      t.text :Decription
+      t.boolean :Public
       t.integer :User_ID
 
       t.timestamps
