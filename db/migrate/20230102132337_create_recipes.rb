f419f51e7949855cee5aaca19112dcc60e9136de
X-Elie-X <103565978+X-Elie-X@users.noreply.github.com>
2023-01-04 16:25:23 +0200

    fixing the log_in

--- db/migrate/20230102132337_create_recipes.rb
+++ db/migrate/20230102132337_create_recipes.rb
@@ -1,7 +1,7 @@
 class CreateRecipes < ActiveRecord::Migration[7.0]
   def change
     create_table :recipes do |t|
-      t.string :Name
+      t.string :name
       t.string :Preparation_Time
       t.string :Cooking_Time
       t.text :Decription
