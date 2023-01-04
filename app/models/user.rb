5fdd3517a30cf32a6909bd151be97f73cc3e0dec
Abigiya Tadesse <Abigiyatadesse23@gmail.com>
2023-01-04 17:38:27 +0300

    Fix linters

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
 
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable, 
-         :confirmable,:trackable,:lockable
+         :recoverable, :rememberable, :validatable,
+         :confirmable, :trackable, :lockable
   has_many :foods
   has_many :recipes
 
