27c71cf69cff1f99a5aa7d21503c3712e8ca3341
Abigiya Tadesse <Abigiyatadesse23@gmail.com>
2023-01-06 19:12:11 +0300

    Add General shopping list page

--- app/controllers/public_recipes_controller.rb
+++ app/controllers/public_recipes_controller.rb
@@ -1,8 +1,5 @@
 class PublicRecipesController < ApplicationController
-  # def index
-  #   @recipes = Recipe.where(public: true).order(created_at: :desc)
-  # end
   def index
-    @recipes = Recipe.where(public: true).order(id: :desc)
+    @recipes = Recipe.where(public: true).order(created_at: :desc)
   end
 end
--- app/controllers/recipe_foods_controller.rb
+++ app/controllers/recipe_foods_controller.rb
@@ -16,26 +16,16 @@ class RecipeFoodsController < ApplicationController
     @recipe_food.recipe_id = params[:recipe_id]
     @recipe_food.food_id = params[:food_id]
     @recipe_food.save
-    redirect_to recipes_path(id: :recipe_id), notice: 'Recipe Food was successfully created.'
-  end
-
-  def update
-    @recipe = Recipe.find(params[:id])
-    if @recipe_food.update
-      flash[:success] = 'Recipe food was successfully updated.'
-      redirect_to recipe_recipe_foods_path(id: :recipe_id)
-    else
-      render :edit, status: :unprocessable_entity
-    end
+    redirect_to recipes_path(id: :recipe_id), notice: 'Recipe Food successfully created.'
   end
 
   def destroy
     @recipe_food = RecipeFood.find_by(id: params[:id])
+
     if @recipe_food.destroy
-      redirect_to recipe_recipe_foods_path(id: :recipe_id)
-      flash[:success] = 'Recipe of food is successfully removed.'
+      redirect_to recipe_recipe_food_path(id: :recipe_id), notice: 'Recipe of food is successfully removed.'
     else
-      flash.now[:error] = 'Recipe food is not removed'
+      redirect_to root_path, alert: 'Recipe food is not removed'
     end
   end
 
