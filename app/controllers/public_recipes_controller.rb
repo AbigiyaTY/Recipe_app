547753ef1ef0f92d7414c931b5b8ea462d71a3c1
X-Elie-X <103565978+X-Elie-X@users.noreply.github.com>
2023-01-06 12:50:01 +0200

    Add publics page

--- app/controllers/public_recipes_controller.rb
+++ app/controllers/public_recipes_controller.rb
@@ -1,4 +1,7 @@
 class PublicRecipesController < ApplicationController
+  # def index
+  #   @recipes = Recipe.where(public: true).order(created_at: :desc)
+  # end
   def index
     @recipes = Recipe.where(public: true).order(id: :desc)
   end
