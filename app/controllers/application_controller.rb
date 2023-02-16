5c8b66f1d55f7c6ecbdb6e273ce9789065e50f10
Abigiya Tadesse <abigiyatadesse23@gmail.com>
2023-02-16 03:57:33 +0300

    Update application_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,15 +1,15 @@
 class ApplicationController < ActionController::Base
-  before_action :authenticate_user!
+#   before_action :authenticate_user!
 
-  # before_action :configure_permitted_parameters, if: :devise_controller?
+#   # before_action :configure_permitted_parameters, if: :devise_controller?
 
-  def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up) do |user_params|
-      user_params.permit(:email, :password, :password_confirmation, :name)
-    end
+#   def configure_permitted_parameters
+#     devise_parameter_sanitizer.permit(:sign_up) do |user_params|
+#       user_params.permit(:email, :password, :password_confirmation, :name)
+#     end
 
-    devise_parameter_sanitizer.permit(:sign_in) do |user_params|
-      user_params.permit(:email, :password, :remember_me)
-    end
-  end
+#     devise_parameter_sanitizer.permit(:sign_in) do |user_params|
+#       user_params.permit(:email, :password, :remember_me)
+#     end
+#   end
 end
