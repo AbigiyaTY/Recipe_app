45212cb094f92247a22c51ed22572b161a918e43
Abigiya Tadesse <abigiyatadesse23@gmail.com>
2023-02-16 03:51:13 +0300

    Update schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -13,7 +13,8 @@
 ActiveRecord::Schema[7.0].define(version: 2023_01_03_185933) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
+  enable_extension "timescaledb"
+  
   create_table "foods", force: :cascade do |t|
     t.string "name"
     t.string "measurement_unit"
